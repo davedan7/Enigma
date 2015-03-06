85c4e59d7fb77afb77cf1fd8ab52890138802dfa
David Daniel <davedan7@gmail.com>
2015-03-05 19:15:10 -0700

    Encoder is working, but needs some serious refeactoring

--- lib/runner.rb
+++ lib/runner.rb
@@ -13,14 +13,37 @@ class Runner
 
 
   def encrypt(string)
-    rotation.generate_key
+    @rotation.generate_key      #Don't generate the key more than once, or everything will explode like a Michael Bay movie
 
-    print "Your key is: #{@rotation.key}"
+    puts "Your key is: #{@rotation.key}"
 
-    string_split = Splitter.new(string)
-    a_encryptor = Encryptor.new(offset.a + rotation.a)
-    a_encryptor.encrypt(string_split.split_a)
+    splitter = Splitter.new(string)
+    splitter.split
+    a_encryptor = Encryptor.new(@offset.a + @rotation.a)
+    a_new = a_encryptor.encrypt(splitter.a)
 
+    b_encryptor = Encryptor.new(@offset.b + @rotation.b)
+    b_new = b_encryptor.encrypt(splitter.b)
+
+    c_encryptor = Encryptor.new(@offset.c + @rotation.c)
+    c_new = c_encryptor.encrypt(splitter.c)
+
+    d_encryptor = Encryptor.new(@offset.d + @rotation.d)
+    d_new = d_encryptor.encrypt(splitter.d)
+
+    encrypted_arr = [a_new, b_new, c_new, d_new]
+
+    new_str = []
+
+    i = 0
+    until a_new[i] == nil
+      print a_new[i] + b_new[i] + c_new[i] + d_new[i] # ~> TypeError: no implicit conversion of nil into String
+      i += 1
+    end
+
+  end
+
+  def decrypt(string, key)
 
   end
 
@@ -29,4 +52,14 @@ class Runner
 end
 
 test = Runner.new
-print test.encrypt("Testing")  # => nil
+test.encrypt("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+
+# >> Your key is: 64571
+# >> 8iu.8iu.8iu.8iu.8iu.8iu.8iu.8iu.8iu.8iu.8iu.8iu.8iu.8iu.8iu.
+
+# ~> TypeError
+# ~> no implicit conversion of nil into String
+# ~>
+# ~> /Users/ddaniel/turing/1module/4week/enigma/lib/runner.rb:41:in `+'
+# ~> /Users/ddaniel/turing/1module/4week/enigma/lib/runner.rb:41:in `encrypt'
+# ~> /Users/ddaniel/turing/1module/4week/enigma/lib/runner.rb:60:in `<main>'
